393ca89f8185fade91ea316e52187fbb1be5f151
Lukas Reinke <post@lukas-reinke.de>
2026-05-13 10:44:10 +0200

    fix migrations bug

--- backend/src/migrations/1747051200000-AddStandupFieldsToUserMeetings.ts
+++ backend/src/migrations/1747051200000-AddStandupFieldsToUserMeetings.ts
@@ -2,14 +2,14 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddStandupFieldsToUserMeetings1747051200000 implements MigrationInterface {
   async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE user_meetings ADD COLUMN IF NOT EXISTS status_last_week TEXT`);
-    await queryRunner.query(`ALTER TABLE user_meetings ADD COLUMN IF NOT EXISTS status_next_week TEXT`);
-    await queryRunner.query(`ALTER TABLE user_meetings ADD COLUMN IF NOT EXISTS status_problems TEXT`);
+    await queryRunner.query(`ALTER TABLE user_meetings ADD COLUMN IF NOT EXISTS "statusLastWeek" TEXT`);
+    await queryRunner.query(`ALTER TABLE user_meetings ADD COLUMN IF NOT EXISTS "statusNextWeek" TEXT`);
+    await queryRunner.query(`ALTER TABLE user_meetings ADD COLUMN IF NOT EXISTS "statusProblems" TEXT`);
   }
 
   async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE user_meetings DROP COLUMN IF EXISTS status_last_week`);
-    await queryRunner.query(`ALTER TABLE user_meetings DROP COLUMN IF EXISTS status_next_week`);
-    await queryRunner.query(`ALTER TABLE user_meetings DROP COLUMN IF EXISTS status_problems`);
+    await queryRunner.query(`ALTER TABLE user_meetings DROP COLUMN IF EXISTS "statusLastWeek"`);
+    await queryRunner.query(`ALTER TABLE user_meetings DROP COLUMN IF EXISTS "statusNextWeek"`);
+    await queryRunner.query(`ALTER TABLE user_meetings DROP COLUMN IF EXISTS "statusProblems"`);
   }
 }
